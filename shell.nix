0850f82dfc9d2e3f842c06a0ad9cef9b6d74ec6a
Jeef <jeffstein@gmail.com>
2026-02-09 17:50:44 -0700

    feat: Migrate to UV

--- shell.nix
+++ shell.nix
@@ -1,27 +1,17 @@
 { pkgs ? import <nixpkgs> { } }:
 
-let
-  pythonEnv = pkgs.python312.withPackages (ps: with ps; [
-    ps.pip
-    ps.numpy
-    ps.uv
-  ]);
-in
 pkgs.mkShell {
   buildInputs = [
-    pythonEnv
-    pkgs.poetry
+    pkgs.python312
+    pkgs.uv
   ];
 
   shellHook = ''
-    export POETRY_HOME=$(pwd)/.poetry
-    export PATH=$POETRY_HOME/bin:$PATH
-    # Optional: install Poetry if not already installed
-    if [ ! -d "$POETRY_HOME" ]; then
-      poetry config virtualenvs.in-project true
-      poetry install
+    # Only create/sync venv if it doesn't exist
+    if [ ! -d ".venv" ]; then
+      uv sync --group dev
     fi
-    # Activate the Poetry environment
+    # Activate the environment
     source .venv/bin/activate
   '';
 }
